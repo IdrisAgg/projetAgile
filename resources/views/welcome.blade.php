c432bf0e5df7c4fe8d993186213787c0684f920a
lina-22 <mustfahinur@gmail.com>
2023-02-20 15:35:09 +0100

    Some Changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -427,6 +427,7 @@
 
                         <div class="ml-12">
                             <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                <h1>Lina22</h1>
                             </div>
                         </div>
                     </div>
