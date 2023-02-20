fcb4ea148a2cc16d3eedadbcaa727c8f7d1b6b48
wahib abbaci <wahabbej@hotmail.fr>
2023-02-20 16:19:02 +0100

    création de l'attribut dete retour dans exemplaire

--- database/migrations/2023_02_20_105604_create_exemplaires_table.php
+++ database/migrations/2023_02_20_105604_create_exemplaires_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('exemplaires', function (Blueprint $table) {
             $table->id();
+            $table->date("date_retour")->nullable();
             $table->foreignId("biblio_id")->constrained("bibliotheques","id")->onDelete("cascade");
             $table->foreignId("ouvrage_id")->constrained("ouvrages","id")->onDelete("cascade");
             $table->foreignId("user_id")->constrained("users","id")->onDelete("cascade")->nullable();
