61cd4baa326d9412c1e3018df8c49f15ea6c49e1
wahib abbaci <wahabbej@hotmail.fr>
2023-02-20 16:01:28 +0100

    supprimer toto

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->boolean("isAdmin");
-            $table->string("toto");
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
