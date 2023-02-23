0e63919e8f16cffd536dc3b65d74d6b3bd90f1ad
lina-22 <haquefahinur@gmail.com>
2023-02-23 12:26:38 +0100

    cleaning the code and improve the view part

--- resources/views/emprunt/show.blade.php
+++ resources/views/emprunt/show.blade.php
@@ -1,44 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container" style="background-color: black; color:rgb(241, 235, 235);">
+    {{-- *********** --}}
+    <div style="">
+        <style>
+            body {
+                background-image: url('/images/books.webp');
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: 1500px 1000Px;
+            }
+        </style>
+    </div>
+    <div class="container"
+        style="text-align: center; background-color: rgb(0, 0, 0); color:aquamarine; font-size:20px; opacity:.8;">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <h2>Borrowed books lists </h2>
-            </div>
-
-            <div>
-                <table data-toggle="table">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Date_retour</th>
-                            <th>Biblio_id</th>
-                            <th>Ouvrage_id</th>
-                            <th>User_id </th>
-                            <th>Created_at</th>
-                            <th>Updated_at</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($examplaires as $oneEmprunt)
-                            <tr>
-                                <td>{{ $oneEmprunt->id }}</td>
-                                <td>{{ $oneEmprunt->date_retour }}</td>
-                                <td>{{ $oneEmprunt->biblio_id }}</td>
-                                <td>{{ $oneEmprunt->ouvrage_id }}</td>
-                                <td>{{ $oneEmprunt->user_id }}</td>
-                                <td>{{ $oneEmprunt->created_at }}</td>
-                                <td>{{ $oneEmprunt->updated_at }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+            <h1>********* LISTER MES EMPRUNTS ********</h1>
+            <hr style="">
         </div>
-
         <div>
+            <table class="table" style="text-align: center; background-color: black; color:aquamarine; height: 500px">
+                <thead>
+                    <tr>
+                        {{-- <th scope="col"><strong>Nom de Ouvrage: </strong></th> --}}
+                        <th scope="col"><strong>Date_retour: </strong></th>
+                        <th scope="col"><strong>Biblio_id: </strong></th>
+                        <th scope="col"><strong>Ouvrage id: </strong></th>
+                        <th scope="col"><strong>User_id: </strong></th>
+                        <th scope="col"><strong>Created_at: </strong></th>
+                        <th scope="col"><strong>Updated_at: </strong></th>
+                        {{-- <button class="btn btn-primary">***</button> --}}
+                    </tr>
+                    <?php
+                        foreach ($examplaires as $oneEmprunt) {
+                            if ($oneEmprunt->user_id !== null){
+                            ?>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?php echo $oneEmprunt->date_retour; ?></td>
+                        <td><?php echo $oneEmprunt->biblio_id; ?></td>
+                        <td><?php echo $oneEmprunt->ouvrage_id; ?></td>
+                        <td><?php echo $oneEmprunt->user_id; ?></td>
+                        <td><?php echo $oneEmprunt->created_at; ?></td>
+                        <td><?php echo $oneEmprunt->updated_at; ?></td>
+                    </tr>
+                </tbody>
+                <?php
+                   }
+                }
+              ?>
+            </table>
         </div>
-
     </div>
 @endsection
