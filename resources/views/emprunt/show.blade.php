a89ceba0395ad4710f9931ed242589ac12964a3d
lina-22 <haquefahinur@gmail.com>
2023-02-23 12:40:35 +0100

    show the book name

--- resources/views/emprunt/show.blade.php
+++ resources/views/emprunt/show.blade.php
@@ -13,7 +13,7 @@
         </style>
     </div>
     <div class="container"
-        style="text-align: center; background-color: rgb(0, 0, 0); color:aquamarine; font-size:20px; opacity:.8;">
+        style="text-align: center; background-color: rgb(0, 0, 0); color:aquamarine; font-size:20px; opacity:.;">
         <div class="row justify-content-center">
             <h1>********* LISTER MES EMPRUNTS ********</h1>
             <hr style="">
@@ -22,7 +22,7 @@
             <table class="table" style="text-align: center; background-color: black; color:aquamarine; height: 500px">
                 <thead>
                     <tr>
-                        {{-- <th scope="col"><strong>Nom de Ouvrage: </strong></th> --}}
+                        <th scope="col"><strong>Nom de Ouvrage: </strong></th>
                         <th scope="col"><strong>Date_retour: </strong></th>
                         <th scope="col"><strong>Biblio_id: </strong></th>
                         <th scope="col"><strong>Ouvrage id: </strong></th>
@@ -38,6 +38,7 @@
                 </thead>
                 <tbody>
                     <tr>
+                        <td><?php echo $oneEmprunt->ouvrage->titre; ?></td>
                         <td><?php echo $oneEmprunt->date_retour; ?></td>
                         <td><?php echo $oneEmprunt->biblio_id; ?></td>
                         <td><?php echo $oneEmprunt->ouvrage_id; ?></td>
