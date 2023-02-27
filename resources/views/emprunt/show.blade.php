971cdce4cc400b321d92f89392cd2fc2e74c4a09
lina-22 <haquefahinur@gmail.com>
2023-02-27 02:39:12 +0100

    Updated the message of conformation

--- resources/views/emprunt/show.blade.php
+++ resources/views/emprunt/show.blade.php
@@ -1,4 +1,3 @@
-<!-- before popup data keep  -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
@@ -22,11 +21,6 @@
             background-repeat: no-repeat;
             /* background-attachment: fixed; */
             background-size: 1500px 1000Px;
-        }
-
-        /* trying to fixed the table head */
-        #myTable {
-            /* table-layout: fixed; */
         }
     </style>
 
@@ -67,34 +61,31 @@
                                     Prolonger
                                 </button>
 
-                                <!-- Modal -->
                                 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
                                     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                                     <div class="modal-dialog modal-dialog-centered" role="document">
                                         <div class="modal-content">
+                                            <h2>Prolonger Des Livre</h2>
+                                            <hr>
                                             <div class="modal-body">
-                                                <h3 style="color:#000">test</h3>
+                                                <h3 style="color:#000">If you want to reserve the book Please Confirm or
+                                                    Cancel</h3>
                                             </div>
                                             <div class="modal-footer">
-                                                <button type="submit" class="btn btn-secondary"
-                                                    data-dismiss="modal">Fermer</button>
+                                                <button type="submit" class="btn btn-danger"
+                                                    data-dismiss="modal">Cancel</button>
                                                 <button type="button" class="btn btn-primary"><a style="color:black"
-                                                        href="/prolonger/{{ $oneEmprunt->id }}">Mettre à jour</a></button>
+                                                        href="/prolonger/{{ $oneEmprunt->id }}">Confirm</a></button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </section>
-                            {{-- <button class="btn btn-info"> <a style="color:black"
-                                    href="/prolonger/{{ $oneEmprunt->id }}">Prolonger</a>
-                                </button> --}}
                         </td>
                     @endif
 
                 </tr>
-
             </tbody>
-
             <?php
                 }
             }
