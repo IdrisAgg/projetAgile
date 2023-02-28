8ddc87c5af3437602b5f0b9174a8de9d8adeadb3
IdrisAgg <agg.idris@gmail.com>
2023-02-28 09:40:28 +0100

    Rafraichissement de la vue Exemplaire/show.blade.php

--- resources/views/exemplaire/show.blade.php
+++ resources/views/exemplaire/show.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
+@section('titre')
+Exemplaires
+@endsection
 @section('content')
 <h1 class="text-center">PAGE DES EXEMPLAIRES</h1>
 <div class="container d-flex justify-content-center col-sm-12 col-md-12 col-xl-12 my-5">
     <table class="table text-center">
         <thead>
-            <tr>
             <tr>
                 <th scope="col"><strong># </strong></th>
                 <th scope="col"><strong>Nom de l'ouvrage </strong></th>
@@ -14,18 +16,15 @@
                 <th scope="col"><strong>DATE RETOUR </strong></th>
                 <th scope="col"><strong>ACTION</strong></th>
             </tr>
-
+        </thead>
+        <tbody>
             <?php
             $num=1;
-
             foreach ($exemplaire as $exemp) {
-                $disponible = 'Disponible';
-                $alert = "success";
+                    $disponible = 'Disponible';
+                    $alert = "success";
             ?>
-        </thead>
-            <tbody>
                 <tr>
-                    <!-- <th>1</th> -->
                     <td><?php echo $num++ ?></td>
                     <td><?php echo $exemp->ouvrage->titre ?></td>
                     <td><?php echo $exemp->bibliotheque->nom ?></td>
@@ -39,7 +38,6 @@
                     <td class="text-{{$alert}} fw-bold">
                        {{$disponible}}</td>
                     <td><strong class=""> reservation</strong></td>
-
                     <td>
                         <div class="">
                             <button class="btn btn-danger">SUPPRIMER</button>
@@ -47,37 +45,10 @@
                         </div>
                     </td>
                 </tr>
-            </tbody>
-
-<?php
-    }
-}
-
-            foreach ($exemplaire as $exemp) {
-                if ($exemp->user_id !== null) {
+            <?php
+                }
             ?>
-        </thead >
-            <tbody class="my-5">
-            <tr>
-                    <!-- <th>1</th> -->
-                    <td><?php echo $num++ ?></td>
-                    <td><?php echo $exemp->ouvrage->titre ?></td>
-                    <td><?php echo $exemp->bibliotheque->nom ?></td>
-                    <td><?php echo $exemp->id ?></td>
-                    <td><strong class="text-danger">RESERVÉ</strong></td>
-                    <td><strong class="text-success">{{$exemp->date_retour}}</strong></td>
-
-                    <td>
-                        <div class="">
-                            <button class="btn btn-danger">SUPPRIMER</button>
-                            <button class="btn btn-primary">MODIFIER</button>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-<?php
-    }
-?>
+        </tbody>
     </table>
 </div>
 @endsection('content')
